0cbdeec0a4bc0a32f05926d929021acf66d3f92e
YanC Lin <yonalin1@gmail.com>
2017-11-17 17:22:47 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -2,7 +2,7 @@ class Fish
   attr_accessor :mood
   attr_reader :name
 
-    def initialize(name,mood = "nervous")
+    def initialize(name,mood="nervous")
         @name = name
         @mood = mood
       end

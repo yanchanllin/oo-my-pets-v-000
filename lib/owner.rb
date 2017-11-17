e7196b3d2ad6341397466000f5828272088c520b
YanC Lin <yonalin1@gmail.com>
2017-11-17 16:41:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,6 +1,6 @@
 class Owner
-  attr_accessor :name
-
+  attr_accessor :name, :pets 
+ attr_reader :species
 
     def initialize(name)
         @name = name
@@ -9,5 +9,26 @@ class Owner
     def all
       self.all = @@all
     end
-
+    def self.reset_all
+      self.clear
+    end 
+    def self.count 
+      
+    end 
+   def species
+     @species = species
+   end 
+    def say_species
+      puts "I am a #{species}."
+    end 
+    def name 
+      self.name 
+    end 
+    def pets 
+      self.pets = {":#{species} => []",":#{species} => []",":#{species} => []"}
+    end 
+    def buy_fish 
+      owner = Owner.new
+      
+    end
 end
